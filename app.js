ddce544be7fe9814a36bbd2305bc80aa1083b9f9
UsmaanMohd <umohd2832@gmail.com>
2026-08-20 15:05:58 +0530

    full-cicd-pipeline

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Simple home route
+//home route
 app.get('/', (req, res) => {
   res.send('Hello! My CI/CD pipeline is working 🎉');
 });
 
-// Health check route (useful for verifying deployment)
+// for verifying deployment
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'App is healthy' });
 });
-
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
